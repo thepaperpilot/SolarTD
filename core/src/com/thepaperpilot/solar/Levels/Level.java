0741d367f729aae20ddfae70d4dba8a138fd2f51
thepaperpilot <thepaperpilot@gmail.com>
2015-12-20 23:09:08 -0600

    Made the path a particle system

--- core/src/com/thepaperpilot/solar/Levels/Level.java
+++ core/src/com/thepaperpilot/solar/Levels/Level.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -18,7 +22,9 @@ import com.thepaperpilot.solar.Main;
 import java.util.ArrayList;
 
 public class Level implements Screen {
+    Batch batch;
     private final static Json json = new Json();
+    private final ParticleEffect pathParticles;
     ArrayList<Tower> towers = new ArrayList<>();
     ShapeRenderer shapeRenderer = new ShapeRenderer();
     float width;
@@ -35,6 +41,26 @@ public class Level implements Screen {
         path = new Point[levelPrototype.path.length];
         for (int i = 0; i < path.length; i++) {
             path[i] = new Point(levelPrototype.path[i]);
+        }
+
+        batch = new SpriteBatch();
+
+        pathParticles = new ParticleEffect();
+        pathParticles.load(Gdx.files.internal("particles/path.p"), Gdx.files.internal("particles/"));
+        for (int i = 0; i < path.length - 1; i++) {
+            if(i != 0) pathParticles.getEmitters().add(new ParticleEmitter(pathParticles.getEmitters().first()));
+            ParticleEmitter emitter = pathParticles.getEmitters().get(i);
+            emitter.getTint().setColors(new float[]{0, 0, 1f});
+            emitter.getAngle().setHigh(new Vector2(path[i+1].x - path[i].x, path[i+1].y - path[i].y).angle());
+            emitter.setPosition(path[i].x, path[i].y);
+            emitter.getLife().setHigh(10 * Vector2.len(path[i+1].x - path[i].x, path[i+1].y - path[i].y));
+        }
+        pathParticles.getEmitters().first().getTint().setColors(new float[]{0, 1f, 0, 0, 0, 1f});
+        pathParticles.getEmitters().first().getTint().setTimeline(new float[]{0, 1});
+        pathParticles.getEmitters().get(pathParticles.getEmitters().size - 1).getTint().setColors(new float[]{0, 0, 1f, 1f, 0, 0});
+        pathParticles.getEmitters().get(pathParticles.getEmitters().size - 1).getTint().setTimeline(new float[]{0, 1});
+        for (int i = 0; i < 100; i++) {
+            pathParticles.update(.1f);
         }
 
         stage = new Stage(new StretchViewport(width, height));
@@ -88,46 +114,15 @@ public class Level implements Screen {
         ui.act(delta);
         ui.draw();
 
-        // TODO make the path into a particle effect
-        // Make the first segment green, middle segments blue, and the last segment red
-        Gdx.gl.glEnable(GL20.GL_BLEND);
         Matrix4 transform = new Matrix4();
         transform.scale(Gdx.graphics.getWidth() / width, Gdx.graphics.getHeight() / height, 1);
+        batch.setTransformMatrix(transform);
+        batch.begin();
+        pathParticles.draw(batch, delta * .2f);
+        batch.end();
+
+        Gdx.gl.glEnable(GL20.GL_BLEND);
         shapeRenderer.setTransformMatrix(transform);
-
-        // omg that is a lot of work for making the paths
-        // paths themselves
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.setColor(0, .805f, .816f, .6f);
-        for (int i = 0; i < path.length - 1; i++) {
-            Vector2 start = new Vector2(path[i].x, path[i].y);
-            Vector2 end = new Vector2(path[i + 1].x, path[i + 1].y);
-            shapeRenderer.rectLine(start, end, 8);
-        }
-        shapeRenderer.end();
-
-        // circles at corners
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(0, .805f, .816f, 1f);
-        for (int i = 1; i < path.length - 1; i++) {
-            shapeRenderer.circle(path[i].x, path[i].y, 5);
-        }
-
-        // arrow showing where enemies enter
-        shapeRenderer.setColor(0, 1, 0, .5f);
-        float dx = path[1].x - path[0].x;
-        float dy = path[1].y - path[0].y;
-        float angle = MathUtils.atan2(dy, dx);
-        shapeRenderer.triangle(path[0].x + 8 * MathUtils.cos(angle - .5f), path[0].y + 8 * MathUtils.sin(angle - .5f), path[0].x + 16 * MathUtils.cos(angle), path[0].y + 16 * MathUtils.sin(angle), path[0].x + 8 * MathUtils.cos(angle + .5f), path[0].y + 8 * MathUtils.sin(angle + .5f));
-
-        // arrow showing where enemies leave
-        shapeRenderer.setColor(1, 0, 0, .5f);
-        dx = path[path.length - 2].x - path[path.length - 1].x;
-        dy = path[path.length - 2].y - path[path.length - 1].y;
-        angle = MathUtils.atan2(dy, dx);
-        Point end = path[path.length - 1];
-        shapeRenderer.triangle(end.x + 16 * MathUtils.cos(angle - .25f), end.y + 16 * MathUtils.sin(angle - .25f), end.x + 8 * MathUtils.cos(angle), end.y + 8 * MathUtils.sin(angle), end.x + 16 * MathUtils.cos(angle + .25f), end.y + 16 * MathUtils.sin(angle + .25f));
-        shapeRenderer.end();
 
         if (selected != null) {
             Gdx.gl20.glLineWidth(4);
@@ -188,6 +183,7 @@ public class Level implements Screen {
     public void dispose() {
         stage.dispose();
         ui.dispose();
+        batch.dispose();
     }
 
 
